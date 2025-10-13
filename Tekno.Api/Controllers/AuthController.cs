97ecd82295ef894b86d837a003282c0d06bb3a7f
duyngulam <neargamer3103@gmail.com>
2025-10-13 23:24:36 +0700

    fix api route

--- Tekno.Api/Controllers/AuthController.cs
+++ Tekno.Api/Controllers/AuthController.cs
@@ -8,7 +8,7 @@ using Tekno.Api.Common.Responses;
 namespace Tekno.Api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/auth")]
     public class AuthController : ControllerBase
     {
         private readonly AuthService _authService;
